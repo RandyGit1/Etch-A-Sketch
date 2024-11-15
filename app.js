4ed59e40dc1ca29415d1cd0c6b828a07fb63503f
RandyGit1 <randy.tan647@gmail.com>
2024-11-14 22:35:51 -0500

    Add toggle pen click event, random rgb color button event, and initial opacity

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 let etch = document.querySelector(".etch");
 const container = document.querySelector(".container");
 const gridButton = document.querySelector(".grid-button");
+const colorButton = document.querySelector(".color-mode");
 const etchDimensions = parseInt(getComputedStyle(etch).width);
+
+const initialSize = 16;
+let draw = false;
+let colorMode = false;
+let opacity = 0;
+
+etch.addEventListener("click", () => draw = !draw);
+colorButton.addEventListener("click", () => colorMode = !colorMode);
+
+createGrid(initialSize, etchDimensions/initialSize);
 
 gridButton.addEventListener("click", ()=>{
     removeGrid();
-    let numSquares = parseInt(prompt("How many squares on each side would you like? (x squares by x)"));
+    let numSquares = parseInt(prompt("How many squares on each side would you like? (x squares by x, limit of 100)"));
+    if (numSquares > 100){
+        numSquares = 100;
+    }
     let squareDimensions = etchDimensions / numSquares;
     createGrid(numSquares, squareDimensions);
 });
@@ -21,6 +35,18 @@ function createGrid(numSquares, squareDimensions){
             let square = document.createElement("div");
             square.classList.add("square");
             square.style.cssText = `width: ${squareDimensions}px; height: ${squareDimensions}px;`;
+            square.addEventListener("mouseenter", () => {
+                if (draw){
+                    opacity += 0.10;
+                    if (colorMode){
+                        square.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${opacity})`;
+                    }
+                    else{
+                        square.style.backgroundColor = `rgb(0, 0, 0, ${opacity})`;
+                    }
+                }
+            });
+                
             squares.push(square);
         } 
         divRow.append(...squares);
