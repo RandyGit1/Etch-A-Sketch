0b7fd80634f317d5e6b57f3c282dd014b3f9a521
RandyGit1 <randy.tan647@gmail.com>
2024-11-14 21:35:18 -0500

    Refactor Grid Creation
    gridButton now listens for square dimensions and uses a new removeGrid function
    before creating a new Grid (now as a function).

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
+let etch = document.querySelector(".etch");
+const container = document.querySelector(".container");
+const gridButton = document.querySelector(".grid-button");
+const etchDimensions = parseInt(getComputedStyle(etch).width);
 
-const etch = document.querySelector(".etch");
-for (let row = 0; row < 16; row++){
-    let divRow = document.createElement("div");
-    divRow.classList.add("row");
+gridButton.addEventListener("click", ()=>{
+    removeGrid();
+    let numSquares = parseInt(prompt("How many squares on each side would you like? (x squares by x)"));
+    let squareDimensions = etchDimensions / numSquares;
+    createGrid(numSquares, squareDimensions);
+});
 
-    let squares = [];
-    for (let col = 0; col < 16; col++){
-        let square = document.createElement("div");
-        square.classList.add("square");
-        squares.push(square);
-    } 
-    divRow.append(...squares);
-    etch.appendChild(divRow);
+
+function createGrid(numSquares, squareDimensions){
+    for (let row = 0; row < numSquares; row++){
+        let divRow = document.createElement("div");
+        divRow.classList.add("row");
+    
+        let squares = [];
+        for (let col = 0; col < numSquares; col++){
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.style.cssText = `width: ${squareDimensions}px; height: ${squareDimensions}px;`;
+            squares.push(square);
+        } 
+        divRow.append(...squares);
+        etch.appendChild(divRow);
+    }
+}
+
+function removeGrid(){
+    etch.innerHTML = "";
 }
 
